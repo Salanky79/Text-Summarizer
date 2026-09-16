66eb587b94121a674e7b66c7ba6329d85a7dc29f
Salanky79 <titque1107@gmail.com>
2026-09-16 21:15:17 +0700

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "Salanky79"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "titque1107@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+# dong goi package nay vao python package index (pypi) de nguoi khac co the su dung package cua minh
